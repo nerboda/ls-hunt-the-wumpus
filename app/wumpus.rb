ddb8cf9c4d6b42cac4951064fd2c4efdfbd32cd2
superchilled <karl@superchilled.co.uk>
2016-09-09 01:12:12 +0000

    Adding adjoining room and move action features

--- app/wumpus.rb
+++ app/wumpus.rb
@@ -1,3 +1,78 @@
 # wumpus.rb
 
+class WumpusGame
+  
+  
+  attr_accessor :player, :map
+  
+  def initialize
+    @map = Map.new
+    @player = Player.new
+    place_player
+  end
+  
+  def place_player
+    player.current_room = map.random_room
+  end
+end
 
+class Map
+  attr_reader :rooms
+  
+  ADJOINING_ROOMS = {
+    1 => [2, 5, 8], 2 => [1, 3, 10], 3 => [2, 4, 12], 4 => [3, 5, 14],
+    5 => [1, 4, 6], 6 => [5, 7, 15], 7 => [6, 8, 17], 8 => [1, 7, 11],
+    9 => [10, 12, 19], 10 => [2, 9, 11], 11 => [8, 10, 20], 12 => [3, 9, 13],
+    13 => [12, 14, 18], 14 => [4, 13, 15], 15 => [6, 14, 16], 16 => [15, 17, 18],
+    17 => [7, 16, 20], 18 => [13, 16, 19], 19 => [9, 18, 20], 20 => [11, 17, 19]
+  }
+  
+  def initialize
+    @rooms = create_rooms
+    # place_hazards
+  end
+
+  def create_rooms
+    rooms = {}
+    (1..20).each do |num| 
+      rooms[num] = Room.new(num, ADJOINING_ROOMS[num])
+    end
+    rooms
+  end
+  
+  def random_room
+    number = rooms.keys.sample
+    rooms[number]
+  end
+end
+
+class Room
+  attr_reader :adjoining_rooms, :number
+  
+  def initialize(num, adjoining_rooms)
+    @number = num
+    @adjoining_rooms = adjoining_rooms
+  end
+  
+end
+
+class Player
+  attr_accessor :current_room
+  
+  def initialize
+    @current_room = nil
+  end
+  
+  def action(choice)
+    if choice == 'move'
+      nearby_rooms_string + "Please choose a room to move to."
+    else
+      ""
+    end
+  end
+  
+  def nearby_rooms_string
+    room_numbers = current_room.adjoining_rooms.join(', ')
+    "Adjoining rooms are #{room_numbers}. "
+  end
+end
--- test/wumpus_test.rb
+++ test/wumpus_test.rb
@@ -7,25 +7,31 @@ require_relative '../app/wumpus'
 
 class WumpusTest < Minitest::Test
   def setup
+  
     @game = WumpusGame.new
+    @map = @game.map
     @player = @game.player
   end
 
   def test_adjoining_room_1
-    assert_equal [2, 5, 8], @game.rooms[1].adjoining_rooms
+    assert_equal [2, 5, 8], @map.rooms[1].adjoining_rooms
   end
 
   def test_adjoining_rooms_2_3_4
-    skip
-    assert_equal [1, 3, 10], @game.rooms[2].adjoining_rooms
-    assert_equal [2, 4, 12], @game.rooms[3].adjoining_rooms
-    assert_equal [3, 5, 14], @game.rooms[4].adjoining_rooms
+    # skip
+    assert_equal [1, 3, 10], @map.rooms[2].adjoining_rooms
+    assert_equal [2, 4, 12], @map.rooms[3].adjoining_rooms
+    assert_equal [3, 5, 14], @map.rooms[4].adjoining_rooms
+  end
+  
+  def test_map_length
+    assert_equal 20, @map.rooms.size
   end
 
   def test_adjoining_rooms_5_to_20
-    skip
-    multiple_adjoining_rooms = (5..20).map do |room| 
-      @game.rooms[room].adjoining_rooms
+    # skip
+    multiple_adjoining_rooms = (5..20).map do |room|
+      @map.rooms[room].adjoining_rooms
     end
     expected_rooms = [
       [1, 4, 6], [5, 7, 15], [6, 8, 17], [1, 7, 11],
@@ -40,50 +46,50 @@ class WumpusTest < Minitest::Test
   # Maybe the movement tests are maybe a little too opinionated?
 
   def test_move_options_room_1
-    skip
-    @player.position = 1
-    action_response = @player.action(m)
-    expected_response = "Adjoining rooms are 2, 5 and 8. "
-      + "Please choose a room to move to."
+    # skip
+    @player.current_room = @map.rooms[1]
+    action_response = @player.action('move')
+    expected_response = "Adjoining rooms are 2, 5, 8. " + 
+      "Please choose a room to move to."
 
     assert_equal expected_response, action_response
   end
 
   def test_move_options_room_2
-    skip
-    @player.position = 2
-    action_response = @player.action(m)
-    expected_response = "Adjoining rooms are 1, 3 and 10. "
-      + "Please choose a room to move to."
+    # skip
+    @player.current_room = @map.rooms[2]
+    action_response = @player.action('move')
+    expected_response = "Adjoining rooms are 1, 3, 10. " + 
+      "Please choose a room to move to."
 
     assert_equal expected_response, action_response
   end
 
   def test_valid_move_room_1_to_2
     skip
-    @player.position = 1
+    @player.current_room = @map.rooms[1]
     move_response = @player.move(2)
-    expected_response = "You are now in room 2. "
-      + "Adjoining rooms are 2, 5 and 8. "
-      + "Do you want to move (m) or shoot (s)?"
+    expected_response = "You are now in room 2. " + 
+      "Adjoining rooms are 2, 5 and 8. " + 
+      "Do you want to move (m) or shoot (s)?"
 
     assert_equal expected_response, move_response
   end
 
   def test_valid_move_room_19_to_20
     skip
-    @player.position = 19
+    @player.current_room = @map.rooms[19]
     move_response = @player.move(20)
-    expected_response = "You are now in room 20. "
-      + "Adjoining rooms are 11, 17 and 19. "
-      + "Do you want to move (m) or shoot (s)?"
+    expected_response = "You are now in room 20. " +
+      "Adjoining rooms are 11, 17 and 19. " +
+      "Do you want to move (m) or shoot (s)?"
 
     assert_equal expected_response, move_response
   end
 
   def test_invalid_move_room_1_to_20
     skip
-    @player.position = 1
+    @player.current_room = @map.rooms[1]
     move_response = @player.move(20)
     expected_response = "You cannot move there! "
       + "Adjoining rooms are 11, 17 and 19. "
